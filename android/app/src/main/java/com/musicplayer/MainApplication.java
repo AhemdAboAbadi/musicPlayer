48f5e0b8db52a61a5811d2b90e11474048c83e83
AhemdAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-12-03 10:15:12 +0200

    add configuration for react native vector icon package

--- android/app/src/main/java/com/musicplayer/MainApplication.java
+++ android/app/src/main/java/com/musicplayer/MainApplication.java
@@ -1,5 +1,7 @@
 package com.musicplayer;
 
+
+import com.oblador.vectoricons.VectorIconsPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          packages.add(new VectorIconsPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
